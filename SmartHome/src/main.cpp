0ab43b163318f18a6d88e779ad5d573947aa5101
Marzyeh Ghayoor <mrzyh.gh11@gmail.com>
2023-01-29 12:44:10 +0330

    Combining all started

---
+++ SmartHome/src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#define Servo 15
+#define LM35 35
+#define WaterSensor 32
+#define Fan 33
+#define Pomp_pool 13
+#define Pompo_Watering 4
+#define SensorPower 14
+#define Tempreture 4
+
+#define RFID_RST 0
+#define RFID_MISO 19
+#define RFID_MOSI 23
+#define RFID_SDK 18
+#define RFID_SDA 5
+
+#define LED1 21
+#define LED2 22
+#define LED3 25
+#define LED4 26
+#define LED5 27
+#define LED6 12
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
